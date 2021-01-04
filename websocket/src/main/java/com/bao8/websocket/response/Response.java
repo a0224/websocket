a9d1a8192751fad675ba2d7341cb3af44d933d9e
wanghao <291162135@qq.com>
2021-01-04 15:21:20 +0800

    websocket

---
+++ websocket/src/main/java/com/bao8/websocket/response/Response.java
@@ -0,0 +1,27 @@
+package com.bao8.websocket.response;
+
+import com.bao8.websocket.dispatcher.IResponseDispatcher;
+import com.bao8.websocket.dispatcher.ResponseDelivery;
+
+/**
+ * WebSocket 响应数据接口
+ */
+public interface Response<T> {
+
+    /**
+     * 获取响应的数据
+     */
+    T getResponseData();
+
+    /**
+     * 设置响应的数据
+     */
+    void setResponseData(T responseData);
+
+    void onResponse(IResponseDispatcher dispatcher, ResponseDelivery delivery);
+
+    /**
+     * 回收资源
+     */
+    void release();
+}
